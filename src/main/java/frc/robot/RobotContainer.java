3781df13e541010a3653af79216db4d2b05ac24f
RD-216FRC <github.robodawgs+216@gmail.com>
2025-03-16 11:10:53 -0400

    Skibidi Rizz Ohio Code de la FIRST

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -109,8 +109,8 @@ public class RobotContainer {
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
-        // Driver.triangle().whileTrue(
-        //     drivetrain.applyRequest(() -> point.withModuleDirection(new Rotation2d(-1, 0)))
+        // Driver.triangle().whileTrue(newSequence(
+        //     drivetrain.applyRequest(() -> point.withModuleDirection(new Rotation2d(-1, 0)),
         //     );
 
         
